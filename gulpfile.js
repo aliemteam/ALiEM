c79a3e852fe23cd073bfe5af9e95ea1437131d9e
Derek P Sifford <dereksifford@gmail.com>
2016-07-21 15:52:45 -0400

    Convert to stylus + add a couple other scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,76 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 const merge = require('merge-stream');
 
+// CSS
+const stylus = require('gulp-stylus');
+const poststylus = require('poststylus');
+const autoprefixer = require('autoprefixer')({ browsers: ['last 2 versions'] });
+const sourcemaps = require('gulp-sourcemaps');
+const insert = require('gulp-insert');
+
+
+// ==================================================
+//                Configurations
+// ==================================================
+
+const jadeConfig = {
+    omitPhpRuntime: true,
+    omitPhpExtractor: true,
+    arraysOnly: false,
+    noArraysOnly: true,
+};
+
+const stylusConfig = {
+    use: [
+        poststylus([
+            autoprefixer,
+        ]),
+    ],
+};
+
+const uglifyConfig  = {
+    compress: {
+        'dead_code': true,
+        'unused': true,
+        'drop_debugger': true,
+        'drop_console': true,
+    },
+};
+
+const styleHeader =
+`/*
+Theme Name: ALiEM
+Template: Avada
+*/
+`
+
+// ==================================================
+//                     Styles
+// ==================================================
+
+gulp.task('stylus:dev', () =>
+    gulp
+       .src('aliem/styles/style.styl', { base: './aliem/styles' })
+       .pipe(sourcemaps.init())
+       .pipe(stylus(stylusConfig))
+       .pipe(sourcemaps.write('.'))
+       .pipe(insert.prepend(styleHeader))
+       .pipe(gulp.dest('dist/aliem'))
+       .pipe(browserSync.stream({ match: '**/*.css' }))
+);
+
+gulp.task('stylus:prod', () =>
+    gulp
+        .src('aliem/styles/style.styl', { base: './aliem/styles' })
+        .pipe(stylus(Object.assign({}, stylusConfig, { compress: true })))
+        .pipe(insert.prepend(styleHeader))
+        .pipe(gulp.dest('dist/aliem'))
+);
+
+
+// ==================================================
+//               Plugin/Theme Fixes
+// ==================================================
 
 gulp.task('fix-theme', () => {
 
