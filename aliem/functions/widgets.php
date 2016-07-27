977cf3109c8a10804e9c140c660ff61e1a58368d
Derek P Sifford <dereksifford@gmail.com>
2016-07-27 14:01:34 -0400

    Add Bookclub widget

--- aliem/functions/widgets.php
+++ aliem/functions/widgets.php
@@ -227,7 +227,68 @@ class AliemPopularPostWidget extends WP_Widget {
 }
 
 
+class BookclubWidget extends WP_Widget {
+
+	public function __construct() {
+		$widgetOps = [
+			'classname' => 'widget widget_text',
+			'description' => 'Bookclub countdown widget',
+		];
+		parent::__construct('bookclub_widget', 'ALiEM: Bookclub Countdown', $widgetOps);
+	}
+
+	public function widget($args, $instance) {
+        extract($args);
+
+        $title = isset($instance['title']) ? $instance['title'] : '';
+		$date = isset($instance['date']) ? $instance['date'] : '';
+
+        echo $before_widget;
+        ?>
+        <div class="heading">
+            <h4 class="widget-title" data-fontsize="13" data-lineheight="18">Book Club Countdown</h4>
+        </div>
+        <div>
+            <img class="alignright" alt="Book Logo" src="/wp-content/uploads/book-sm.png" width="52" height="56" />
+            <strong>Next book</strong>: <a href="https://aliem.com/aliem-book-club/"><?php echo $title ?></a><br>
+
+            <strong>Discussion: </strong><?php echo $date ?><br>
+            <strong>Twitter:</strong> <a href="http://www.twitter.com/aliembook" target="_blank">@ALiEMbook</a>
+        </div>
+        <?php
+        echo $after_widget;
+	}
+
+	public function form($instance) {
+        $defaults = array(
+			'title' => '',
+			'date' => '',
+		);
+		$instance = wp_parse_args((array) $instance, $defaults); ?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php esc_attr_e('Next book title:', 'Avada'); ?></label>
+			<input class="large-text" type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('date'); ?>"><?php esc_attr_e('Date:', 'Avada'); ?></label>
+			<input class="large-text" type="text" id="<?php echo $this->get_field_id('date'); ?>" name="<?php echo $this->get_field_name('date'); ?>" value="<?php echo $instance['date']; ?>" />
+		</p>
+		<?php
+	}
+
+	public function update($new, $old) {
+        $instance = $old;
+
+		$instance['title'] = $new['title'];
+		$instance['date'] = $new['date'];
+
+		return $instance;
+	}
+}
+
+
 
 add_action('widgets_init', function () {
     register_widget('AliemPopularPostWidget');
+    register_widget('BookclubWidget');
 });
