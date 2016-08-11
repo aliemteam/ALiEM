174aa4eca1b9f06f96a37d151926f1b0ad26cfec
Derek P Sifford <dereksifford@gmail.com>
2016-08-10 23:08:29 -0400

    Add custom social icons + SVG shortcodes

--- aliem/functions/index.php
+++ aliem/functions/index.php
@@ -1,5 +1,10 @@
 <?php
 
+function aliem_js() {
+    wp_enqueue_script('printfriendly', 'https://pf-cdn.printfriendly.com/ssl/main.js');
+}
+add_action('wp_enqueue_scripts', 'aliem_js');
+
 require_once(dirname(__FILE__) . '/legacy.php');
 require_once(dirname(__FILE__) . '/shortcodes.php');
 require_once(dirname(__FILE__) . '/misc.php');
--- aliem/functions/shortcodes.php
+++ aliem/functions/shortcodes.php
@@ -9,3 +9,37 @@ function imgbox($atts) {
 	return '<div class="img-box"><img style="width: 100%;" src="' . $a['url'] . '" />' . $a['title'] . '</div>';
 }
 add_shortcode('img_box', 'imgbox');
+
+// SVG shortcode (for ALiEM series')
+function aliem_svg_generator($atts) {
+    $a = shortcode_atts([
+        'series' => 'aliem',
+        'width' => 'auto',
+        'height' => 'auto',
+        'align' => 'none',
+    ], $atts);
+    extract($a);
+
+    $filenames = [
+        'aliem' => 'aliem-logo-horizontal.svg',
+        'healthyinem' => 'stay-healthy-logo.svg',
+        'bookclub' => 'bookclub-book.svg',
+        'newsubmissions' => 'submissions-pencil.svg',
+        'fincubator' => 'fincubator-logo.svg',
+        'facubator' => 'facubator-logo.svg',
+        'crincubator' => 'crincubator-logo.svg',
+        'airseries' => 'air-logo.svg',
+        'airseries-pro' => 'air-pro-logo.svg',
+        'aliemu' => 'aliemu-logo-horizontal.svg',
+    ];
+
+    $url = '/wp-content/themes/aliem/assets/' . $filenames[$series];
+
+    if ($align === 'center') {
+        return "<div style='text-align: center; width: 100%;'><img src='$url' style='width: $width; height: $height;' /></div>";
+    }
+
+    return "<img src='$url' style='width: $width; height: $height; float: $align; padding: 10px;' />";
+
+}
+add_shortcode('svg', 'aliem_svg_generator');
