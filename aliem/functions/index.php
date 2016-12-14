7da8c162fc18de4d104baef5325ee83e99a99931
Derek P Sifford <dereksifford@gmail.com>
2016-12-13 20:13:41 -0500

    Finish adding svg support

--- aliem/functions/index.php
+++ aliem/functions/index.php
@@ -11,3 +11,4 @@ require_once(dirname(__FILE__) . '/shortcodes.php');
 require_once(dirname(__FILE__) . '/misc.php');
 require_once(dirname(__FILE__) . '/widgets.php');
 require_once(dirname(__FILE__) . '/avada.php');
+require_once(dirname(__FILE__) . '/class-svg-support.php');
