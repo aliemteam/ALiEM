09a3ad173042a878203beb0c001e5e2781f17e6c
Derek P Sifford <dereksifford@gmail.com>
2017-03-28 22:16:40 -0400

    add custom button shortcode

---
+++ aliem/lib/shortcodes/button.php
@@ -0,0 +1,19 @@
+<?php
+
+function aliem_button($atts = [], $content = null) {
+    $atts = shortcode_atts([
+        'href' => '',
+    ], $atts);
+    extract($atts);
+
+    if ($href === '') {
+        return "<h1 style='color: red'>Buttons requrie a 'href' attribute!</h1>";
+    }
+
+    return "
+    <a href='$href' target='_blank' rel='noopener noreferrer' class='btn btn--primary'>
+        $content
+    </a>
+    ";
+}
+add_shortcode('button', 'aliem_button');
--- aliem/lib/shortcodes/index.php
+++ aliem/lib/shortcodes/index.php
@@ -3,6 +3,7 @@
 require_once(dirname(__FILE__) . '/book-cover.php');
 require_once(dirname(__FILE__) . '/flip-box.php');
 require_once(dirname(__FILE__) . '/box.php');
+require_once(dirname(__FILE__) . '/button.php');
 require_once(dirname(__FILE__) . '/plot.php');
 
 require_once(dirname(__FILE__) . '/stubs.php');
