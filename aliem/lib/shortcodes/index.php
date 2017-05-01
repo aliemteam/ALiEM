13b44c1ad2613e1fffad59c35f4799695c0f4130
Derek P Sifford <dereksifford@gmail.com>
2017-05-01 17:47:24 -0400

    add 'person' shortcode

--- aliem/lib/shortcodes/index.php
+++ aliem/lib/shortcodes/index.php
@@ -5,6 +5,7 @@ require_once(dirname(__FILE__) . '/box.php');
 require_once(dirname(__FILE__) . '/flex.php');
 require_once(dirname(__FILE__) . '/flip-box.php');
 require_once(dirname(__FILE__) . '/button.php');
+require_once(dirname(__FILE__) . '/person.php');
 require_once(dirname(__FILE__) . '/plot.php');
 
 require_once(dirname(__FILE__) . '/stubs.php');
---
+++ aliem/lib/shortcodes/person.php
@@ -0,0 +1,39 @@
+<?php
+
+function aliem_person($atts = [], $content = null) {
+    $atts = shortcode_atts([
+        'name' => '',
+        'title' => '',
+        'image' => '',
+        'twitter' => '',
+    ], $atts);
+    extract($atts);
+
+    if ($name === '' || $image === '' || $title === '') {
+        return "<h1 style='color: red'>Person shortcode requires 'name', 'image', and 'title' attributes!</h1>";
+    }
+
+    if ($twitter !== '') {
+        $twitter = "
+        <div class='person__twitter'>
+            <a aria-label='fusion-twitter' href='https://twitter.com/$twitter' target='_blank' rel='noopener noreferrer'></a>
+        </div>";
+    }
+
+    return "
+    <div class='person'>
+        <div class='person__img'>
+            <img src='$image'/>
+        </div>
+        <div class='person__details'>
+            <div class='person__meta'>
+                <div class='person__name'>$name</div>
+                <div class='person__title'>$title</div>
+            </div>
+            $twitter
+        </div>
+        <div class='person__background'>$content</div>
+    </div>
+    ";
+}
+add_shortcode('person', 'aliem_person');
