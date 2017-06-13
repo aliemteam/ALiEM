e76601597aa45367602d9a24bec9fd7cdd015b58
Derek P Sifford <dereksifford@gmail.com>
2017-06-12 23:07:01 -0400

    various server/infrastructure updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,27 @@
 const autoprefixer = require('autoprefixer-stylus');
-// const sourcemaps = require('gulp-sourcemaps');
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const imagemin = require('gulp-imagemin');
-// const replace = require('gulp-replace');
 const del = require('del');
-const { exec } = require('child_process');
 const merge = require('merge-stream');
 const browserSync = require('browser-sync').create();
 
-// ==================================================
-//                Configurations
-// ==================================================
-
 const stylusConfig = {
-    use: [
-        autoprefixer({ browsers: 'last 2 versions' }),
-    ],
+    use: [autoprefixer({ browsers: 'last 2 versions' })],
 };
 
-// ==================================================
-//                     Utility
-// ==================================================
-gulp.task('del', done => (
-    del(['dist/aliem/**', '!dist/aliem']).then(() => done())
-));
+gulp.task('del', done =>
+    del(['dist/aliem/**', '!dist/aliem']).then(() => done()),
+);
 
-gulp.task('reload', (done) => { browserSync.reload(); done(); });
-
-gulp.task('chown', (done) => {
-    exec("ls -l dist/ | awk '{print $3}' | tail -n -1", (err, stdout) => {
-        if (stdout.trim() === process.env.USER) {
-            done();
-            return;
-        }
-        exec(`sudo chown -R ${process.env.USER} ${process.env.PWD}`, () => {
-            done();
-        });
-    });
+gulp.task('reload', done => {
+    browserSync.reload();
+    done();
 });
 
-// ==================================================
-//                     Static
-// ==================================================
 gulp.task('static', () => {
     const php = gulp
-        .src([
-            'aliem/**/*.php',
-            'aliem/**/*.css',
-        ], { base: './' })
+        .src(['aliem/**/*.php', 'aliem/**/*.css'], { base: './' })
         .pipe(gulp.dest('dist'));
 
     const svg = gulp
@@ -63,44 +36,52 @@ gulp.task('static', () => {
     return merge(php, svg, vendor);
 });
 
-// ==================================================
-//                     Styles
-// ==================================================
-
-gulp.task('stylus:dev', () => (
-    gulp.src([
-        'aliem/styles/style.styl',
-        'aliem/styles/editor.styl',
-        'aliem/styles/admin.styl',
-    ], { base: './aliem/styles' })
-    .pipe(stylus(stylusConfig))
-    .pipe(gulp.dest('dist/aliem'))
-    .pipe(browserSync.stream())
-));
-
-gulp.task('stylus:prod', () => (
+gulp.task('stylus:dev', () =>
     gulp
-        .src([
-            'aliem/styles/style.styl',
-            'aliem/styles/editor.styl',
-            'aliem/styles/admin.styl',
-        ], { base: './aliem/styles' })
-        .pipe(stylus(Object.assign({}, stylusConfig, { compress: true })))
+        .src(
+            [
+                'aliem/styles/style.styl',
+                'aliem/styles/editor.styl',
+                'aliem/styles/admin.styl',
+            ],
+            { base: './aliem/styles' },
+        )
+        .pipe(stylus(stylusConfig))
         .pipe(gulp.dest('dist/aliem'))
-));
+        .pipe(browserSync.stream()),
+);
 
-gulp.task('_build', gulp.series('chown', 'del', gulp.parallel('static', 'stylus:prod')));
+gulp.task('stylus:prod', () =>
+    gulp
+        .src(
+            [
+                'aliem/styles/style.styl',
+                'aliem/styles/editor.styl',
+                'aliem/styles/admin.styl',
+            ],
+            { base: './aliem/styles' },
+        )
+        .pipe(stylus(Object.assign({}, stylusConfig, { compress: true })))
+        .pipe(gulp.dest('dist/aliem')),
+);
 
-gulp.task('_dev', gulp.series('chown', 'del', gulp.parallel('static', 'stylus:dev'), dev));
+gulp.task('_build', gulp.series('del', gulp.parallel('static', 'stylus:prod')));
+
+gulp.task(
+    '_dev',
+    gulp.series('del', gulp.parallel('static', 'stylus:dev'), dev),
+);
 
 function dev() {
     browserSync.init({
         proxy: 'localhost:8080',
     });
 
-    gulp.watch('aliem/styles/**/*.styl', { queue: false }, gulp.series('stylus:dev'));
+    gulp.watch(
+        'aliem/styles/**/*.styl',
+        { queue: false },
+        gulp.series('stylus:dev'),
+    );
 
-    gulp.watch([
-        'aliem/**/*.php',
-    ], gulp.series('static', 'reload'));
+    gulp.watch(['aliem/**/*.php'], gulp.series('static', 'reload'));
 }
