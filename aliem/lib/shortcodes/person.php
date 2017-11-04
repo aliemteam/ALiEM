dd767f19512c8e2cec05929ca7e5b5f82b638fe9
Derek P Sifford <dereksifford@gmail.com>
2017-11-04 18:39:42 -0400

    fix person shortcode issues

--- aliem/lib/shortcodes/person.php
+++ aliem/lib/shortcodes/person.php
@@ -8,8 +8,15 @@ function person($atts = [], $content = null) {
         'title' => '',
         'image' => '',
         'twitter' => '',
+        'align' => '',
     ], $atts);
     extract($atts);
+
+    if (!in_array($align, ['left', 'right'])) {
+        $style = '';
+    } else {
+        $style = "float: $align; margin: 0 20px; margin-$align: 0;";
+    }
 
     if ($name === '' || $image === '' || $title === '') {
         return "<h1 style='color: red'>Person shortcode requires 'name', 'image', and 'title' attributes!</h1>";
@@ -23,7 +30,7 @@ function person($atts = [], $content = null) {
     }
 
     return "
-    <div class='person'>
+    <div class='person' style='$style'>
         <div class='person__img'>
             <img src='$image'/>
         </div>
