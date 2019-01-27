89baf215927dca140fe6ead58ddbce82b0dd015a
Derek P Sifford <dereksifford@gmail.com>
2019-01-27 00:34:34 -0500

    add terraform file

---
+++ terraform.tf
@@ -0,0 +1,56 @@
+terraform {
+  backend "local" {
+    path = "lib/terraform.tfstate"
+  }
+}
+
+output "droplet_name" {
+  value = "${module.digitalocean_web.name}"
+}
+
+output "droplet_ip" {
+  value = "${module.digitalocean_web.ipv4_address}"
+}
+
+output "droplet_region" {
+  value = "${module.digitalocean_web.region}"
+}
+
+variable "do_token" {
+  description = "DigitalOcean Access Token."
+}
+
+variable "cf_token" {
+  description = "Cloudflare API Key."
+}
+
+locals {
+  domain = "aliem.com"
+}
+
+provider "cloudflare" {
+  version = "~> 1.11"
+
+  email = "mlin@aliem.com"
+  token = "${var.cf_token}"
+}
+
+provider "digitalocean" {
+  version = "~> 1.1"
+
+  token = "${var.do_token}"
+}
+
+module "digitalocean_web" {
+  source = "github.com/aliemteam/infrastructure//compute/digitalocean/wordpress"
+
+  domain = "${local.domain}"
+  image  = "docker-18-04"
+}
+
+module "cloudflare_dns" {
+  source = "github.com/aliemteam/infrastructure//networking/cloudflare/dns"
+
+  domain       = "${local.domain}"
+  ipv4_address = "${module.digitalocean_web.ipv4_address}"
+}
