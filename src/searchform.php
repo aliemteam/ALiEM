1d29f5e34d497ec2e2fc1e469772af7ed604e946
Derek Sifford <dereksifford@gmail.com>
2019-02-12 15:05:21 -0500

    improve 404 page

---
+++ src/searchform.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Search form template.
+ *
+ * @package ALiEM
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<form role="search" class="searchform" method="get" action="/">
+	<input type="text" name="s" class="s" placeholder="Search..." aria-label="Search ALIEM.com" aria-required="true" required />
+	<button class="btn btn--primary" style="margin: 0;">Search</button>
+</form>
